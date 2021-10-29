c5e7afcb6c95d68d64745a9a7f63e59cf05583e1
Austin Casteel <austin.casteel@gmail.com>
2021-10-29 11:55:02 -0500

    dynamic

--- main/io.js
+++ main/io.js
@@ -1,6 +1,7 @@
 const { app, dialog } = require("electron");
 const fs = require("fs-extra");
 const path = require("path");
+const dns = require('dns');
 
 // local dependencies
 const notification = require("./notification");
@@ -75,33 +76,46 @@ exports.addVpn = (username, password, config) => {
 // };
 
 exports.addOrg = () => {
-  value = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+  segment1 = "segmented01.csdsuite.com"
+  segment2 = "segmented11.csdsuite.com"
+  dns.resolveTxt(segment1, (err, data1) => {
+    if(err) throw err;
+    dns.resolveTxt(segment2, (err, data2) => {
+      if(err) throw err;
+      pushResults(data1, data2);
+    });
+  });
 
-xpMEYXg0+xMFK4EEACMEIwQBJz8wx6NRJkdAKwMAsa2qnU6KC7yFwHJ+GAps
-t0G1BQGkjzjNHauVgRieYN+A00Q2BlDZTMRa6WGPkQu7tGHrRY0AjxIS0Y34
-71MsdweCkjC+jOuGqOaCdH+vaAwrxjeYfme5b6XlIbUIS3m3yTQwHxP8Ush0
-rg7KCkFBDvDek1qUHw/NIEdsb2JhbCA8c3VwcG9ydEB0ZWNoc2xheWVycy5j
-b20+wsAPBBATCgAdBQJheDT7BAsJBwgDFQgKBBYCAQACGQECGwMCHgEAIQkQ
-9n7PV/lVyusWIQQbekFdH0Pc19OYmv32fs9X+VXK69uWAgkBrYZxZvsu+LeK
-VXUO0XApcLVQdXlbnzZUEWn1Nw8pDIhRVq3ex3cfwM1KSc0aP1WJ0Btnnu3I
-NwWkJcJ3jD0AlIMCCMObntPb280+8UG08gSslo75RkSeAYIPqDjTErS/KVZn
-NGhLb+5TeVaZspFWKJ3CdJvot+mh8+jpIofhvojTHcd+zpcEYXg0+xIFK4EE
-ACMEIwQBjx3aUfri5xO0sv30nQ9RhXvZU+fMcpixFjlf4CSuiWG/11MkYNwy
-hqHK4QdyjgjVRPbsQOCaQ0MnNTlqstYDWtQBz9LKfGmNna+4aEXZxmpN4Cht
-JZXuGnhCieMGTSVl3o+eMJQ5oU9PtDdO3gQGSsT7tOAjIfE+LkpnYum7ZdI/
-098DAQoJwrsEGBMKAAkFAmF4NPsCGwwAIQkQ9n7PV/lVyusWIQQbekFdH0Pc
-19OYmv32fs9X+VXK676lAgizscaFEW/WF7PzQRom94FPQQ7fF+4AptjdrY73
-9cMSatKYK5/hyXIPqSjuP9PvoJk/N4GDuePJw6oIkgtXTZcO7gIJAdITIiRx
-BU55YDESKmC3LwdCOhkjMs0zvm3dsx1dq6YpSjEAohi4lf28Gh6cjVNSyhaI
-lQhZIUqEhX3F6puqQs3s =/Ims
------END PGP PUBLIC KEY BLOCK-----`
-  const keyPath1 = path.resolve(orgDir, `ts-pub.asc`);
-  if (!fs.existsSync(keyPath1)) {
-    fs.writeFileSync(keyPath1, value);
-  }
-  const keyPath2 = path.resolve(orgDir, `ts-pvt.asc`);
-  if (!fs.existsSync(keyPath2)) {
-    fs.writeFileSync(keyPath2, value);
+  function pushResults(data1, data2){
+    var seg1 = data1;
+    var seg2 = data2;
+    var seg11 = seg1.toString();
+    var seg22 = seg2.toString();
+    var seg111 = seg11.replace(/,/g, '');
+    var seg222 = seg22.replace(/,/g, '');
+    let com = seg111.concat(seg222);
+    let com1 = com.slice(36,-34);
+    function addNewlines(str) {
+      var result = '';
+      while (str.length > 0) {
+        result += str.substring(0, 60) + '\n';
+        str = str.substring(60);
+      }
+      return result;
+    }
+    var com2 = addNewlines(com1);
+    var header = "-----BEGIN PGP PUBLIC KEY BLOCK-----"
+    var footer = "-----END PGP PUBLIC KEY BLOCK-----"
+    var nl = "\n"
+    var com3 = header+nl+nl+com2+footer;
+    const keyPath1 = path.resolve(orgDir, `ts-pub.asc`);
+    if (!fs.existsSync(keyPath1)) {
+      fs.writeFileSync(keyPath1, com3);
+    }
+    const keyPath2 = path.resolve(orgDir, `ts-pvt.asc`);
+    if (!fs.existsSync(keyPath2)) {
+      fs.writeFileSync(keyPath2, com3);
+    }
   }
 };
 
@@ -113,87 +127,40 @@ exports.removeOrg = () => {
 };
 
 exports.getOrg = () => {
-  // dns.resolveTxt(host1, (err, data) => data1 = data);
-  // dns.resolveTxt(host2, (err, data) => data2 = data);
+  // segment1 = "segmented01.csdsuite.com"
+  // segment2 = "segmented11.csdsuite.com"
+  // dns.resolveTxt(segment1, (err, data1) => {
+  //   if(err) throw err;
+  //   dns.resolveTxt(segment2, (err, data2) => {
+  //     if(err) throw err;
+  //     pushResults(data1, data2);
+  //   });
+  // });
 
-  // dns.resolveTxt(host1, (err, data) => console.log(data));
-  // dns.resolveTxt(host2, (err, data) => console.log(data));
-
-
-
-  // value = `-----BEGIN PGP PUBLIC KEY BLOCK-----
-
-  // xpMEYXg0+xMFK4EEACMEIwQBJz8wx6NRJkdAKwMAsa2qnU6KC7yFwHJ+GAps
-  // t0G1BQGkjzjNHauVgRieYN+A00Q2BlDZTMRa6WGPkQu7tGHrRY0AjxIS0Y34
-  // 71MsdweCkjC+jOuGqOaCdH+vaAwrxjeYfme5b6XlIbUIS3m3yTQwHxP8Ush0
-  // rg7KCkFBDvDek1qUHw/NIEdsb2JhbCA8c3VwcG9ydEB0ZWNoc2xheWVycy5j
-  // b20+wsAPBBATCgAdBQJheDT7BAsJBwgDFQgKBBYCAQACGQECGwMCHgEAIQkQ
-  // 9n7PV/lVyusWIQQbekFdH0Pc19OYmv32fs9X+VXK69uWAgkBrYZxZvsu+LeK
-  // VXUO0XApcLVQdXlbnzZUEWn1Nw8pDIhRVq3ex3cfwM1KSc0aP1WJ0Btnnu3I
-  // NwWkJcJ3jD0AlIMCCMObntPb280+8UG08gSslo75RkSeAYIPqDjTErS/KVZn
-  // NGhLb+5TeVaZspFWKJ3CdJvot+mh8+jpIofhvojTHcd+zpcEYXg0+xIFK4EE
-  // ACMEIwQBjx3aUfri5xO0sv30nQ9RhXvZU+fMcpixFjlf4CSuiWG/11MkYNwy
-  // hqHK4QdyjgjVRPbsQOCaQ0MnNTlqstYDWtQBz9LKfGmNna+4aEXZxmpN4Cht
-  // JZXuGnhCieMGTSVl3o+eMJQ5oU9PtDdO3gQGSsT7tOAjIfE+LkpnYum7ZdI/
-  // 098DAQoJwrsEGBMKAAkFAmF4NPsCGwwAIQkQ9n7PV/lVyusWIQQbekFdH0Pc
-  // 19OYmv32fs9X+VXK676lAgizscaFEW/WF7PzQRom94FPQQ7fF+4AptjdrY73
-  // 9cMSatKYK5/hyXIPqSjuP9PvoJk/N4GDuePJw6oIkgtXTZcO7gIJAdITIiRx
-  // BU55YDESKmC3LwdCOhkjMs0zvm3dsx1dq6YpSjEAohi4lf28Gh6cjVNSyhaI
-  // lQhZIUqEhX3F6puqQs3s =/Ims
-  // -----END PGP PUBLIC KEY BLOCK-----`
-
-  // console.log(value)
-  // return value
-
-
-
-  // console.log(data1);
-  // console.log(data2);
-
-  // function keyPostProc(value1, value2) {
-  //   var returnedData = {};
-  //   returnedData["segment1"] = value1;
-  //   returnedData["segment2"] = value2;
-  //   return returnedData;
+  // function pushResults(data1, data2){
+  //   var seg1 = data1;
+  //   var seg2 = data2;
+  //   var seg11 = seg1.toString();
+  //   var seg22 = seg2.toString();
+  //   var seg111 = seg11.replace(/,/g, '');
+  //   var seg222 = seg22.replace(/,/g, '');
+  //   let com = seg111.concat(seg222);
+  //   let com1 = com.slice(36,-34);
+  //   function addNewlines(str) {
+  //     var result = '';
+  //     while (str.length > 0) {
+  //       result += str.substring(0, 60) + '\n';
+  //       str = str.substring(60);
+  //     }
+  //     return result;
+  //   }
+  //   var com2 = addNewlines(com1);
+  //   var header = "-----BEGIN PGP PUBLIC KEY BLOCK-----"
+  //   var footer = "-----END PGP PUBLIC KEY BLOCK-----"
+  //   var nl = "\n"
+  //   var com3 = header+nl+nl+com2+footer;
+  //   console.log(com3);
   // }
-  // var returnValue = keyPostProc(data1, data2)
-
-  // console.log(returnValue)
-  
-  //return returnValue
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // dns.resolveTxt('segmented11.csdsuite.com', (err, addresses) => {
-  //   if (err) {
-  //     console.log("DNS Error: "+err);
-  //   }
-  //   const segment2 = addresses;
-  //   console.log("segment2: "+segment2);
-  // });
-  // dns.resolveTxt('segmented01.csdsuite.com', (err, addresses) => {
-  //   if (err) {
-  //     console.log("DNS Error: "+err);
-  //   }
-  //   const segment1 = addresses;
-  //   console.log("segment1: "+segment1);
-  // });
 };
 
 exports.tsZip = () => {
